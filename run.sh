fe3e0cf9f503ac50d670b78f729de67099b01476
Enes Kılıçaslan <eeneskilicaslan@gmail.com>
2016-12-25 10:29:09 +0100

    delete comment

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #compile cpp files  // -W -Wall -Werror -pedantic 
 g++ Dictionary.cpp main.cpp PostingList.cpp Spimi.cpp Token.cpp -o exec
 
-#/media/psf/Home/Desktop/au/IR/project/en/
 python parser.py #run the parser
 ./exec #run spimi
